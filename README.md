1cce28955e4bb61d55dc7240f76f07d47f316e13
Nancy Liliana Sáenz Moreno <111588657+n4ncy27@users.noreply.github.com>
2022-09-29 10:01:32 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejercicio No. 25: hallar la suma de los n primeros numeros enteros 
+
+## Diagrama de flujo
+
+![Diagrama de flujo](diagrama.png) "Diagrama de flujo")
---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+# hallar la suma de los n primeros numeros enteros 
+# input 
+N = int (input("Digite N: "))
+
+#procesing
+i = 1 
+sum = 0
+while i <= N:
+    sum = sum + i 
+    i = i + 1
+
+#output 
+print (" La suma es: "  )
